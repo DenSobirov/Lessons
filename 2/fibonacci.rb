fe0377c3bcb402fec64e153f0750cf6ca1687ed4
DenSobirov <denis.sobirov@gotoinc.eu>
2019-07-23 16:33:40 +0300

    Added fixes

--- 2/fibonacci.rb
+++ 2/fibonacci.rb
@@ -1,7 +1,11 @@
-fibonacci = [1, 2]
-loop do 
-  number = fibonacci[-1] + fibonacci[-2]
-  number > 100 ? break : fibonacci << number
+fibonacci = []
+loop do
+  if fibonacci.count < 2
+    fibonacci << 1
+  else
+    number = fibonacci[-1] + fibonacci[-2]
+    number > 100 ? break : fibonacci << number
+  end
 end
 
 p fibonacci
--- 2/goods.rb
+++ 2/goods.rb
@@ -1,8 +1,8 @@
 goods = {}
-loop do 
+loop do
   puts 'Введите имя товара:'
   name = gets.chomp
-  break if name == 'стоп'
+  break if name == 'stop'
   puts 'Введите стоимость товара:'
   cost = gets.chomp.to_f
   puts 'Введите количество единиц товара'
@@ -11,9 +11,8 @@ loop do
   goods[name] = {cost: cost, count: count}
 end
 
-total_sum = 0
-goods.each do |name, att|
-  puts "item: #{name}, cost: #{att[:cost]*att[:count]}"
-  total_sum += att[:cost]*att[:count]
+a = goods.inject(0) do |total, (name, (att))|
+ p "item: #{name}, cost: #{att[:cost] * att[:count]}"
+ total + (att[:cost] * att[:count])
 end
-p "Total: #{total_sum}"
+p "total: #{a}$"
