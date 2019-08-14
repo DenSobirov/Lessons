62e5739262d7c61c33e519dc61cefe0e465291a0
DenSobirov <denis.sobirov@gotoinc.eu>
2019-08-14 22:59:37 +0300

    Resolved task with area of triangle

---
+++ 1/area_of_triangle.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Area of triangle. Program requests base and height
+# of triangle and calculating the area.
+
+puts 'Hello, please put base if triangle'
+base = gets.chomp.to_i
+puts 'And height of the same triangle'
+height = gets.chomp.to_i
+
+puts "squre of triangle is #{base * height / 2.0}"
